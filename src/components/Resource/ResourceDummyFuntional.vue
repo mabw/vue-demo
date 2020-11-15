9434ef7d3bffe953ffc2f44316d355e86eddb89a
Marvin <uutar@qq.com>
2020-11-15 23:23:16 +0800

    Add functional demo

---
+++ src/components/Resource/ResourceDummyFuntional.vue
@@ -0,0 +1,10 @@
+<template functional>
+  <div>
+    <button @click="parent.toggleComponent('ResourcePanel')">
+      {{ props.value === "" ? "please select a value" : props.value }}
+    </button>
+    <button @click="parent.toggleComponent('ResourcePanel2')">
+      {{ props.value2 === "" ? "please select another value" : props.value2 }}
+    </button>
+  </div>
+</template>
